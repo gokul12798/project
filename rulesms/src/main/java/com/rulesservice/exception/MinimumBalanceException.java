a6529762518fa7cfed9f1dc8de435fbe75073723
Krishna <31002079+Krish225@users.noreply.github.com>
2021-04-10 18:46:02 +0530

    Add files via upload

---
+++ rulesms/src/main/java/com/rulesservice/exception/MinimumBalanceException.java
@@ -0,0 +1,17 @@
+package com.rulesservice.exception;
+
+public class MinimumBalanceException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public MinimumBalanceException() {
+		super();
+	}
+
+	public MinimumBalanceException(String message) {
+		super(message);
+	}
+
+	
+	
+}
